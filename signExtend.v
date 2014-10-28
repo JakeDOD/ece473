c7f153615466a66eabf7fc989a96f032cef38653
JakeDOD <jakedalyodonnell@gmail.com>
2014-10-28 11:16:07 -0400

    Updated sign extend

--- signExtend.v
+++ signExtend.v
@@ -5,9 +5,7 @@ module signExtend(
 	output reg [31:0] dataOut);
 
 	always @(dataIn) begin
-		//if (dataIn[15] == 1'b1) begin
-			dataOut = { {16{dataIn[15]}}, dataIn[15:0] };
-		//end
+		dataOut = { {16{dataIn[15]}}, dataIn[15:0] };
 	end
 
 endmodule
