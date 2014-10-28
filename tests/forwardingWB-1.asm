ccffff5404e028d118520aa78cd3d5ded3fd930e
JakeDOD <jakedalyodonnell@gmail.com>
2014-10-28 13:41:41 -0400

    Updated some asm files with comments

--- tests/forwardingWB-1.asm
+++ tests/forwardingWB-1.asm
@@ -1,7 +1,7 @@
-add	$t7, $t8, $t8
-add	$t5, $t6, $t2
-add	$t2, $t7, $t4
-add	$t8, $t3, $t5
+add	$t7, $t8, $t8	// $15 = 48
+add	$t5, $t6, $t2	// $13 = 24
+add	$t2, $t7, $t4	// $10 = 60
+add	$t8, $t3, $t5	// $24 = 35
 nop
 nop
 nop
