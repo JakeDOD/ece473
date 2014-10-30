91118e5e93db7451486bed223213318e03c6cce0
JakeDOD <jakedalyodonnell@gmail.com>
2014-10-30 15:55:21 -0400

    Added blinkin' lights!

---
+++ BlinkinLights.v
@@ -0,0 +1,19 @@
+// BlinkinLights.v
+
+module BlinkinLights(
+	input wire clock,
+	output reg[27:0] leds);
+	
+	initial begin
+		leds = 1;
+	end
+	
+	always @(clock) begin
+		if (leds == 134217728) begin
+			leds = 0;
+		end else begin
+			leds = leds + 1;
+		end
+	end
+	
+endmodule
