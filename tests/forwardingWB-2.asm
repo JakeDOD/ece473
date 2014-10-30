494ac5432aa74a5912bd4a60944391a0dd27097b
Jeremiah Simonsen <jeremiah.simonsen@maine.edu>
2014-10-30 16:38:18 -0400

    Updated test cases

--- tests/forwardingWB-2.asm
+++ tests/forwardingWB-2.asm
@@ -1,7 +1,7 @@
-add	$t4, $t9, $t8
-add	$t5, $t6, $t2
-add	$t2, $t4, $t4
-add	$t8, $t5, $t5
+add	$t4, $t9, $t8	# 12 = 49
+add	$t5, $t6, $t2	# 13 = 24
+add	$t2, $t4, $t4	# 10 = 24
+add	$t8, $t5, $t5	# 24 = 26
 nop
 nop
 nop
--- tests/testProgram1.asm
+++ tests/testProgram1.asm
@@ -1,6 +1,6 @@
-add	$t0, $t1, $t2
-add	$t3, $t4, $t5
-add	$t6, $t7, $s0
+add	$t0, $t1, $t2	# $8 = 19
+add	$t3, $t4, $t5	# $11 = 25
+add	$t6, $t7, $s0	# 14 = 31
 nop
 nop
 nop
